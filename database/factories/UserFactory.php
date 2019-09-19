69f2a0b103b050e7a66dd75a9f75997969657b10
Your Name <you@example.com>
2019-09-20 00:19:01 +0600

    UsersTableSeeder added

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Models\User;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name('male'),
+        'email' => $faker->email,
+        'phone_number' =>$faker->phoneNumber,
+        'password' => bcrypt('123456'),
+        'email_verified_at' => Carbon::now(),
+
+    ];
+});
