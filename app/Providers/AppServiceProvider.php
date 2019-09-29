c066493ae5b794c8ed3a21cdee75c96bb82de47c
Your Name <you@example.com>
2019-09-29 11:47:06 +0600

    categories list added in menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -26,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $categories = Category:: select(['name', 'slug'])->where('category_id', null)->get() ;
+        view()->share('categories',$categories);
+
         Schema::defaultStringLength(191);
     }
 }
--- resources/views/frontend/partials/_header.blade.php
+++ resources/views/frontend/partials/_header.blade.php
@@ -17,9 +17,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
-                    <h4 class="text-white">About</h4>
-                    <p class="text-muted">Laravel Ecommerce management system platform .</p>
+                    <h4 class="text-white">Categories</h4>
+                    <ul class="list-unstyled">
+
+                        @foreach($categories as $category)
+
+                        <li>
+                            <a href="{{$category->slug}}" class="">{{$category->name}} </a>
+                        </li>
+                        @endforeach
+
+                    </ul>
                 </div>
+
                 <div class="col-sm-4 offset-md-1 py-4">
                     <h4 class="text-white">Menu</h4>
                     <ul class="list-unstyled">
